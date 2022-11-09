b76ede6ba1e99ad91cd241f2459c1976a6ec4948
lucas <fernandes20lucas@gmail.com>
2022-11-09 10:14:14 -0300

    feat: interface for Vehicle with zod schema

---
+++ src/interfaces/IVehicle.ts
@@ -0,0 +1,11 @@
+import { z } from 'zod';
+
+const zodSchema = z.object({
+  model: z.string().min(3),
+  color: z.string().min(3),
+  status: z.boolean().optional(),
+  year: z.number().positive().gte(1900).lte(2022),
+  buyValue: z.number().positive(),
+});
+
+export type IVehicle = z.infer<typeof zodSchema>;
